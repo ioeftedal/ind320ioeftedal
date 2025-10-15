81e2cda71fe17b40bbde1218c3f985b578eaec22
Ivar Eftedal <ivareftedal@Ivars-MacBook-Pro.local>
2025-10-15 10:14:21 +0200

    feat: flake int

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ] (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          uv
+          python312
+          basedpyright
+          ruff
+          heroku
+        ];
+
+        # LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.stdenv.cc.cc];
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+      };
+    });
+}
