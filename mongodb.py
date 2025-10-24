263c2c25625b1e89f319871132c110defb0ba550
Ivar Eftedal <ivareftedal@Ivars-MacBook-Pro.local>
2025-10-24 19:22:41 +0200

    feat: funtion to fetch data from mongodb

---
+++ mongodb.py
@@ -0,0 +1,21 @@
+# mongodb.py
+import streamlit as st
+import pymongo
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient(st.secrets["mongo"]["uri"])
+
+client = init_connection()
+
+# Pull data from the collection.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data():
+    db = client['elhub_data']
+    collection = db['consumption_2021']
+    items = collection.find()
+    items = list(items)
+    return items
