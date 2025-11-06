67653afb72d8214b278fa5e64b35f67d3d64b1e1
Ivar Eftedal <ivareftedal@Ivars-MacBook-Pro.local>
2025-11-06 17:01:37 +0100

    feat: updated for comments that i got for CA2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 home_page = st.Page("pages/home.py", title="Home Page", icon=":material/house:")
 table_page = st.Page("pages/table.py", title="Table Page", icon=":material/table:")
 plot_page = st.Page("pages/plot.py", title="Plot Page", icon=":material/chart_data:")
-dummy_page = st.Page("pages/dummy.py", title="Dummy Page", icon=":material/robot_2:")
 elhub_api_page = st.Page("pages/elhubapi.py", title="Elhub API")
+dummy_page = st.Page("pages/dummy.py", title="Dummy Page", icon=":material/robot_2:")
 
-pg = st.navigation([home_page, table_page, plot_page, dummy_page, elhub_api_page])
+pg = st.navigation([home_page, table_page, plot_page, elhub_api_page, dummy_page])
 st.set_page_config(page_title="IND320 Eftedal", page_icon=":material/edit:")
 pg.run()
